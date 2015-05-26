a460f589eba464771b71448c97c582cd907f728e
norrmana <andre.norrman@gmail.com>
2015-05-26 19:58:10 +0200

    First commit...

---
+++ HelloGitWorld/src/org/norrmana/gitworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.norrmana.gitworld;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println(new Date().toString());
+	}
+
+}
