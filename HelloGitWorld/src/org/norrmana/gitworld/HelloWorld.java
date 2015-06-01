82957055780afe632db073f9ea1b4950a8ac557a
André Norrman <andre.norrman@cgi.com>
2015-06-01 09:03:33 +0200

    First MVU change

--- HelloGitWorld/src/org/norrmana/gitworld/HelloWorld.java
+++ HelloGitWorld/src/org/norrmana/gitworld/HelloWorld.java
@@ -4,8 +4,14 @@ import java.util.Date;
 
 public class HelloWorld {
 
+	/**
+	 * Test
+	 * MVU-first change
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println(new Date().toString());
+		
 	}
 
 }
